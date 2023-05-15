4915d02af905b04bfcfc555d65fb271d6a97d335
Joaquin Parra <you@example.com>
2023-05-15 20:39:10 -0300

    agregando import firebase en app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+    apiKey: "AIzaSyCvIh1gzMUbdTybOaNC60sJlisPRUqrFEA",
+    authDomain: "esiar-f601e.firebaseapp.com",
+    projectId: "esiar-f601e",
+    storageBucket: "esiar-f601e.appspot.com",
+    messagingSenderId: "373460662675",
+    appId: "1:373460662675:web:bdd82d7e0aad5711021066",
+    measurementId: "G-BKSD2CT2YD"
+};
 
 function App() {
   return (
