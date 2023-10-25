dd5f06d520636e3bb6e1ee5651274c6c2296fbaf
Joaquin Parra <you@example.com>
2023-10-24 21:59:18 -0300

    agregando funcion para controlar cerrar sesion

--- src/contexts/authContext/authContext.jsx
+++ src/contexts/authContext/authContext.jsx
@@ -1,4 +1,11 @@
 import {auth} from "../../components/firebaseConfig/firebaseConfig"
+import { createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword, 
+    GoogleAuthProvider, 
+    signInWithPopup,
+    GitHubAuthProvider,
+signOut,
+ } from "firebase/auth";
 import { createContext, useContext } from "react"
 // permite acceder al contexto de autenticación en tus componentes de React.
 
@@ -15,5 +22,46 @@ export const useAuth = () => {
 
 //componente para globalizar el contexto y compartir info entre componenetes
 export function AuthProvider({ children }){
-    return <authContext.Provider> {children} </authContext.Provider>
+
+//funcion para controlar registro
+const register = async (email, password) => {
+    const response = await createUserWithEmailAndPassword(auth, email, password)
+    console.log(response);
+};
+
+//funcion para controlar sesion ya iniciada
+const login = async (email, password)=>{
+    const response = await signInWithEmailAndPassword(auth, email, password)
+console.log(response)
+}
+
+//funcion para controlar iniciar sesion con google
+const loginWithGoogle = async () =>{
+    const responseGoogle = new GoogleAuthProvider()
+    return await signInWithPopup(auth, responseGoogle)
+}
+
+//funcion para controlar iniciar sesion con Git Hub
+const loginWithGitHub = async () =>{
+    const responseGitHub = new GitHubAuthProvider()
+    return await signInWithPopup(auth, responseGitHub)
+}
+
+//funcion para controlar cerrar sesion
+const logout = async () =>{
+    const response = await signOut(auth)
+    console.log(response)
+}
+
+
+
+
+    return <authContext.Provider value={(
+        register,
+        login,
+        loginWithGoogle,
+        loginWithGitHub,
+        logout )}
+        > 
+        {children} </authContext.Provider>
     }
