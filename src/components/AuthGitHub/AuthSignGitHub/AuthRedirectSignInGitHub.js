7809ce6189d5e53701b822aeec0028caf04a77d5
Joaquin Parra <you@example.com>
2023-11-14 17:57:05 -0300

    agregando comentarios a AuthGitHub

--- src/components/AuthGitHub/AuthSignGitHub/AuthRedirectSignInGitHub.js
+++ src/components/AuthGitHub/AuthSignGitHub/AuthRedirectSignInGitHub.js
@@ -1,4 +1,5 @@
 import { getAuth, signInWithRedirect } from "firebase/auth";
+//el redireccionamiento a la página de acceso, llama a signInWithRedirect
 
 const auth = getAuth();
 signInWithRedirect(auth, provider);
--- src/components/AuthGoogle/AuthProviderGoogle/AuthProviderGoogle.js
+++ src/components/AuthGoogle/AuthProviderGoogle/AuthProviderGoogle.js
@@ -1,3 +1,3 @@
 import { GoogleAuthProvider } from "firebase/auth";
-
+//Crea una instancia del objeto del proveedor de Google para 
 const provider = new GoogleAuthProvider();
