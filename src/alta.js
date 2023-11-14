7e7e054831c981b84193a3a6fb843a206e9ec814
Fedest01 <federico.estevez.epet20@gmail.com>
2023-11-14 18:46:07 -0300

    configurando alertas

--- src/alta.js
+++ src/alta.js
@@ -4,9 +4,10 @@ function Alta() {
             contraseña: document.getElementById("contraseña").value,
         })
         .then((docRef) => {
-            console.log("Document written with ID: ", docRef.id);
+            alert("Registro exitoso");
+
         })
         .catch((error) => {
-            console.error("Error adding document: ", error);
+            alert("Error en el registro");
         });
 }
