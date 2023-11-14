35a27b297812dae27e7e65b026637ad4904a516a
Joaquin Parra <you@example.com>
2023-11-14 17:20:58 -0300

    agregando comentarios a authUserSessionActived

--- src/components/AuthUser/AuthUserSessionActived/AuthUserCurrent.js
+++ src/components/AuthUser/AuthUserSessionActived/AuthUserCurrent.js
@@ -1,14 +1,12 @@
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    const uid = user.uid;
-    // ...
-  } else {
-    // User is signed out
-    // ...
-  }
-});
+const user = auth.currentUser;
+//Obtén el usuario con sesión activa con currentUser, donde si no accedio nadie , el valor de currentUser es nulo.
+if (user) {
+  // User is signed in, see docs for a list of available properties
+  // https://firebase.google.com/docs/reference/js/auth.user
+  // ...
+} else {
+  // No user is signed in.
+}
