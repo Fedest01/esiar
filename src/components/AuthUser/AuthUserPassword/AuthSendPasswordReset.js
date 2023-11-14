7116a773cbf5d4b6db78671204b9aed30281d9ab
Joaquin Parra <you@example.com>
2023-11-14 17:31:57 -0300

    agregando comentarios a AuthUserPassword

--- src/components/AuthUser/AuthUserPassword/AuthSendPasswordReset.js
+++ src/components/AuthUser/AuthUserPassword/AuthSendPasswordReset.js
@@ -1,5 +1,5 @@
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
-
+//enviar un correo electrónico de restablecimiento de contraseña a un usuario, usando el método sendPasswordResetEmail
 const auth = getAuth();
 sendPasswordResetEmail(auth, email)
   .then(() => {
