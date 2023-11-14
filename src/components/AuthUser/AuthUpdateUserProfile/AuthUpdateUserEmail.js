5848ce6754f17e49034340dc2f9d34365617fbc1
Joaquin Parra <you@example.com>
2023-11-14 17:26:57 -0300

    agregando comentarios a authUpdateUserProfile y Email

--- src/components/AuthUser/AuthUpdateUserProfile/AuthUpdateUserEmail.js
+++ src/components/AuthUser/AuthUpdateUserProfile/AuthUpdateUserEmail.js
@@ -1,4 +1,5 @@
 import { getAuth, updateEmail } from "firebase/auth";
+//configurar la dirección de correo electrónico de un usuario con el método updateEmail
 const auth = getAuth();
 updateEmail(auth.currentUser, "user@example.com").then(() => {
   // Email updated!
