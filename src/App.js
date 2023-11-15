b0871ae1e378b5128026c0ef4a1b7cddcf237e73
Joaquin Parra <you@example.com>
2023-11-15 17:17:23 -0300

    agregando login a app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react'; // Asegúrate de importar React
 
 import { AuthProvider } from './contexts/authContext/authContext';
 import { Registro } from './components/agregarUsuario/agregarUsuario';
-
+import { Login } from './components/Login/login';
 
 function App() {
   return (
     <AuthProvider>
       <div className='app'>
         <h1>Esiar</h1>
-        <Registro />
+        <Login />
       </div>
     </AuthProvider>
   );
