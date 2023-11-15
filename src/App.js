6fff324f94981a08289ab54d1f638a1d19bd29df
Joaquin Parra <you@example.com>
2023-11-15 16:05:36 -0300

    agregando formulario de login y registro

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'; // Asegúrate de importar React
 
 import { AuthProvider } from './contexts/authContext/authContext';
 import { Registro } from './components/agregarUsuario/agregarUsuario';
+import { formulario } from './components/formulario/formulario';
 
 function App() {
   return (
     <AuthProvider>
       <div className='app'>
-        <h1>Esiar</h1>
-        <Registro />
+       <h2 class="titulo">Esi<span class="tituloDos">AR</span></h2>
+       <h2 class="eslogan">Descubre mas sobre la Educacion Sexual Integral</h2>
+          
+          <formulario />
       </div>
     </AuthProvider>
   );
---
+++ src/components/formulario/formulario.jsx
@@ -0,0 +1,56 @@
+export function formulario(){
+    return(
+        //LOGIN Y REGISTRO 
+    <div class="container" id="container">
+    
+    //FORMULARIO REGISTRO 
+    <div class="form-container sign-up-container">
+       <form action="#">
+           <h1 class="registrar">Registrate</h1>
+           <input type="text" placeholder="Nombre" />
+           <input type="email" placeholder="Email" />
+           <input type="password" placeholder="Contraseña" />
+           <button>Crear cuenta</button>
+       </form>
+    </div>
+    
+    //FORMULARIO INICIAR SESION 
+    <div class="form-container sign-in-container">
+       <form action="#">
+           <h1>Inicia sesion</h1>
+           <div class="social-container">
+               <a href="#" class="social anonimo"><i class="fab fa-creative-commons-by"></i></a>
+               <a href="#" class="social google"><i class="fab fa-google-plus-g"></i></a>
+               <a href="#" class="social github"><i class="fab fa-github"></i></a>
+           </div>
+           <span>o ingresa con</span>
+           <input type="email" placeholder="Email" />
+           <input type="password" placeholder="Contraseña" />
+           
+           <button>Iniciar sesion</button>
+       </form>
+    </div>
+    
+    //PANELES DE ACCESO A INICIAR SESION Y REGISTRO
+    <div class="overlay-container">
+       <div class="overlay">
+    
+           //ACCESO A PANEL INICIAR SESION 
+           <div class="overlay-panel overlay-left">
+               <h1>¿Ya tienes una cuenta?</h1>
+               <p>Inicia sesion con tu cuenta para poder seguir descubriendo mas sobre ESI </p>
+               <button class="ghost" id="signIn">Inicia Sesion</button>
+           </div>
+           
+           //ACCESO A PANEL DE REGISTRO 
+           <div class="overlay-panel overlay-right">
+               <h1>No tenes cuenta?</h1>
+               <p>Registrate para poder seguir descubriendo mas sobre ESI </p>
+               <button class="ghost" id="signUp">Registrate</button>
+           </div>
+    
+       </div>
+    </div>
+    </div>
+    )
+    }
