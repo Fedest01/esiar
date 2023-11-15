728044d29dfdea45d1d9e9921c30d698503dd0d7
Joaquin Parra <you@example.com>
2023-11-15 16:07:44 -0300

    modificando formulario de login y registro

--- src/components/formulario/formulario.js
+++ src/components/formulario/formulario.js
@@ -1,9 +1,9 @@
 export function formulario(){
 return(
-    //LOGIN Y REGISTRO 
+
 <div class="container" id="container">
 
-//FORMULARIO REGISTRO 
+
 <div class="form-container sign-up-container">
    <form action="#">
        <h1 class="registrar">Registrate</h1>
@@ -14,7 +14,7 @@ return(
    </form>
 </div>
 
-//FORMULARIO INICIAR SESION 
+
 <div class="form-container sign-in-container">
    <form action="#">
        <h1>Inicia sesion</h1>
@@ -31,18 +31,17 @@ return(
    </form>
 </div>
 
-//PANELES DE ACCESO A INICIAR SESION Y REGISTRO
+
 <div class="overlay-container">
    <div class="overlay">
-
-       //ACCESO A PANEL INICIAR SESION 
+ 
        <div class="overlay-panel overlay-left">
            <h1>¿Ya tienes una cuenta?</h1>
            <p>Inicia sesion con tu cuenta para poder seguir descubriendo mas sobre ESI </p>
            <button class="ghost" id="signIn">Inicia Sesion</button>
        </div>
        
-       //ACCESO A PANEL DE REGISTRO 
+     
        <div class="overlay-panel overlay-right">
            <h1>No tenes cuenta?</h1>
            <p>Registrate para poder seguir descubriendo mas sobre ESI </p>
