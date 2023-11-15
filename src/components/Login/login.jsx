69037dc46804ca74da771277d89a0a143a18dbcb
Joaquin Parra <you@example.com>
2023-11-15 18:31:02 -0300

    agregando diferentes tipo de inicio de sesion en login

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 import { faGoogle,faGithub } from '@fortawesome/free-brands-svg-icons';
 import appFirebase from '../firebaseConfig/firebaseConfig.js';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
@@ -17,7 +18,8 @@ function Login(props) {
     const togglePasswordVisibility = () => {
       setShowPassword(!showPassword);
     };
-  
+
+  // constante de autenticacion de correo, nombre y contrasena
     const functAutenticacion = async (e) =>{  
         e.preventDefault();
         const correo = e.target.email.value;
@@ -32,4 +34,38 @@ function Login(props) {
           }
         
     }
-  
+}
+// Constante para iniciar sesion con google
+const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);  // Puedes acceder a la información del usuario aquí
+        navigate("/Home");
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+// Constante para iniciar sesion con github
+  const signInWithGithub = () => {
+    const provider = new GithubAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);  // Puedes acceder a la información del usuario aquí
+        navigate("/Home");
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  };
+// Constante para iniciar sesion con github
+  const signInAnonimo = () => {
+    signInAnonymously(auth)
+    .then((result) => {
+        console.log(result.user);  // Puedes acceder a la información del usuario aquí
+        navigate("/Home");
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  };
+
