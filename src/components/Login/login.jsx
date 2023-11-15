5ccf83c64d1a047332891e6bbb1d73ed3497bc47
Joaquin Parra <you@example.com>
2023-11-15 19:10:00 -0300

    desacrgando dependencias

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -3,8 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 import { faGoogle,faGithub } from '@fortawesome/free-brands-svg-icons';
 import appFirebase from '../firebaseConfig/firebaseConfig';
+import { Link, useNavigate } from 'react-router-dom';
+
+
 import { doc, setDoc, getDocs, collection, query, where, getFirestore } from "firebase/firestore"; 
 import { getAuth, signInWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword } from 'firebase/auth'
+const auth = getAuth(appFirebase);
 
 function Login(props) {
     const navigate = useNavigate();
@@ -15,7 +19,8 @@ function Login(props) {
     const togglePasswordVisibility = () => {
       setShowPassword(!showPassword);
     };
-  
+
+  // constante de autenticacion de correo, nombre y contrasena
     const functAutenticacion = async (e) =>{  
         e.preventDefault();
         const correo = e.target.email.value;
@@ -24,80 +29,98 @@ function Login(props) {
           try {
             await signInWithEmailAndPassword(auth, correo, contraseña);
             navigate("/Home");
-            console.log("Iniciado sesion con exito!!")
+            console.log("Iniciado sesion con exito")
           } catch (error) {
-            alert ("El correo es incorrecto");
+            alert ("El correo electronico es incorrecto");
           }
         
     }
-  
-    const signInWithGoogle = () => {
-      const provider = new GoogleAuthProvider();
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          console.log(result.user);  // Puedes acceder a la información del usuario aquí
-          navigate("/Home");
-        }).catch((error) => {
-          console.log(error.message);
-        });
+
+// Constante para iniciar sesion con google
+const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);  // Puedes acceder a la información del usuario aquí
+        navigate("/Home");
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+// Constante para iniciar sesion con github
+  const signInWithGithub = () => {
+    const provider = new GithubAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);  // Puedes acceder a la información del usuario aquí
+        navigate("/Home");
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  };
+// Constante para iniciar sesion con anonimo
+  const signInAnonimo = () => {
+    signInAnonimo(auth)
+    .then((result) => {
+        console.log(result.user);  // Puedes acceder a la información del usuario aquí
+        navigate("/Home");
+      }).catch((error) => {
+        console.log(error.message);
+      });
     };
-    
-    const signInWithGithub = () => {
-      const provider = new GithubAuthProvider();
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          console.log(result.user);  // Puedes acceder a la información del usuario aquí
-          navigate("/Home");
-        }).catch((error) => {
-          console.log(error.message);
-        });
-    };
-  
-      return (
-      <>
-      <div className="form-container">
-          <h2 className='form-title'>Iniciar Sesion</h2>
-  
-          <form onSubmit={functAutenticacion}>
-  
-            <Label className="info-label" infoLabel="Ingrese email"/>
-            <input className='input-field' type='text' placeholder='Ingresar email' id='email'/>
-                         
-            <Label className="info-label" infoLabel="Ingrese contraseña"/>
-            <input className='input-field' type={showPassword ? "text" : "password"} placeholder='Ingresar contraseña ' id='password'/>
-            <button
-            type="button"
-            className="verPassword"
-            onClick={togglePasswordVisibility}
-            >
-              c
-            </button>
-            <button className='button'>Inicia Sesion</button>
-            
-          </form>
-  
-          <button onClick={signInWithGoogle} className='google-signin-button'>
-            <FontAwesomeIcon icon={faGoogle} />
-            <span>Iniciar sesión con Google</span>
-          </button>    
-  
-          <button onClick={signInWithGithub} className='google-signin-button'>
-            <FontAwesomeIcon icon={faGithub} />
-            <span>Iniciar sesión con GitHub</span>
+
+return (
+    <>
+    <div className="form-container">
+        <h2 className='form-title'>Iniciar Sesion</h2>
+
+        <form onSubmit={functAutenticacion}>
+
+          <Label className="info-label" infoLabel="Ingrese email"/>
+          <input className='input-field' type='text' placeholder='Ingresar email' id='email'/>
+                       
+          <Label className="info-label" infoLabel="Ingrese contraseña"/>
+          <input className='input-field' type={showPassword ? "text" : "password"} placeholder='Ingresar contraseña ' id='password'/>
+          <button
+          type="button"
+          className="verPassword"
+          onClick={togglePasswordVisibility}
+          >
+            c
           </button>
-  
-          <h6 className='form-title'>
-              <Link to='/Recuper-Contraseña' className='button'>
-               ¿Olvidaste tu contraseña?
-              </Link>
-          </h6>
-          <h4 className='form-title'> ¿No tienes cuenta?
-              <Link to='/Registro' className='button'>
-                  Registrate
-              </Link>
-          </h4>
-        </div>
-      </>
-      );
-      }
-      export default Login;
+          <button className='button'>Inicia Sesion</button>
+          
+        </form>
+
+        <button onClick={signInWithGoogle} className='google-signin-button'>
+          <FontAwesomeIcon icon={faGoogle} />
+          <span>Iniciar sesión con Google</span>
+        </button>    
+
+        <button onClick={signInWithGithub} className='google-signin-button'>
+          <FontAwesomeIcon icon={faGithub} />
+          <span>Iniciar sesión con GitHub</span>
+        </button>
+
+        <button onClick={signInAnonimo} className='google-signin-button'>
+          <FontAwesomeIcon icon={faUserSecret} />
+          <span>Iniciar sesión en anonimo</span>
+        </button>
+
+        <h6 className='form-title'>
+            <Link to='/Recuper-Contraseña' className='button'>
+             ¿Olvidaste tu contraseña?
+            </Link>
+        </h6>
+        <h4 className='form-title'> ¿No tienes cuenta?
+            <Link to='/Registro' className='button'>
+                Registrate
+            </Link>
+        </h4>
+      </div>
+    </>
+    );
+}
+  export default Login;
+
